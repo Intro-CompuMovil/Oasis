905b38568baa78f86208cb038018d13d528bd10c
CaMoraG <camoraq83@gmail.com>
2024-11-18 21:06:44 -0500

    Arreglos carga de productos

--- app/src/main/java/com/example/oasis/logica/comprador/CompradorInicio.kt
+++ app/src/main/java/com/example/oasis/logica/comprador/CompradorInicio.kt
@@ -7,6 +7,7 @@ import android.view.inputmethod.EditorInfo
 import android.widget.EditText
 import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.oasis.MainActivity
@@ -14,6 +15,7 @@ import com.example.oasis.R
 import com.example.oasis.logica.adapters.CategoryAdapter
 import com.example.oasis.logica.db.DataBaseSimulator
 import com.example.oasis.logica.db.FireBaseDataBase
+import com.example.oasis.logica.utility.AppUtilityHelper
 import com.example.oasis.logica.utility.UIHelper
 import com.example.oasis.model.Category
 import com.example.oasis.model.Comprador
@@ -21,6 +23,7 @@ import com.example.oasis.model.Order
 import com.example.oasis.model.Product
 import com.example.oasis.model.Solicitud
 import com.example.oasis.model.Ubicacion
+import kotlinx.coroutines.launch
 import java.time.LocalDateTime
 
 
@@ -46,7 +49,7 @@ class CompradorInicio : AppCompatActivity() {
     private fun initUI(){
         initBuscar()
         initButtons()
-        initRecyclerView()
+        getCategories()
     }
 
     private fun initBuscar(){
@@ -70,29 +73,24 @@ class CompradorInicio : AppCompatActivity() {
         }
     }
 
-    private fun initRecyclerView() {
-        getCategories { categoryList ->
-            val rvCategories = findViewById<RecyclerView>(R.id.rvCategories)
-            val categoryAdapter = CategoryAdapter(this, categoryList)
-            rvCategories.layoutManager = LinearLayoutManager(this)
-            rvCategories.adapter = categoryAdapter
-        }
+    private fun initRecyclerView(categorias: List<Category>) {
+        val rvCategories = findViewById<RecyclerView>(R.id.rvCategories)
+        val categoryAdapter = CategoryAdapter(this, categorias)
+        rvCategories.layoutManager = LinearLayoutManager(this)
+        rvCategories.adapter = categoryAdapter
     }
 
-    private fun getCategories(onCategoriesLoaded: (List<Category>) -> Unit) {
-        val productos = mutableListOf<Product>()
+    private fun getCategories() {
+        lifecycleScope.launch {
+            val productos = FireBaseDataBase().obtenerProductos()
 
-        val producto = Producto()
-        producto.obtenerProductosDesdeFirebase { productoList ->
-            // Una vez que tenemos los productos desde Firebase
-            productos.addAll(productoList)
-
-            // Agrupamos los productos por categoría
-            val categoryList = productos.groupBy { it.getCategoria() }
-                .map { Category(it.key, it.value) }
-
-            // Llamamos al callback para devolver las categorías
-            onCategoriesLoaded(categoryList)
+            if (productos.isNotEmpty()) {
+                val categoryList = productos.groupBy { it.getCategoria() }
+                    .map { Category(it.key, it.value) }
+                initRecyclerView(categoryList)
+            }else{
+                AppUtilityHelper.showErrorDialog(this@CompradorInicio, "No se encontraron productos")
+            }
         }
     }
 
